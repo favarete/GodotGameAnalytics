a4e9f50611659873d5a15c0708496c9598f3a7b5
favarete <rodrigo.favarete@hotmail.com>
2017-04-10 02:43:50 -0300

    Add new feature 'Progression event'

--- GodotGameAnalytics/android/src/GodotGameAnalytics.java
+++ GodotGameAnalytics/android/src/GodotGameAnalytics.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 
 //import GameAnalytics SDK
 import com.gameanalytics.sdk.GameAnalytics;
+import com.gameanalytics.sdk.GAProgressionStatus;
 
 /**
  * Created by rodrigo favarete on April 2, 2017
@@ -38,7 +39,7 @@ public class GodotGameAnalytics extends Godot.SingletonBase
     }
 
     /**
-     * initOnUiThread
+     * Initialize the SDK
      */
 
     public void initOnUiThread(String gaGameKey, String gaSecretKey) 
@@ -82,7 +83,7 @@ public class GodotGameAnalytics extends Godot.SingletonBase
     }
 
     /**
-     * If manual session handling is enabled and SDK is initialized ("init" method will start a session automatically)
+     * If manual session handling is enabled and SDK is initialized ("init" method will start a session automatically),
      * you can control the beggining of a session from here
      */
 
@@ -97,6 +98,7 @@ public class GodotGameAnalytics extends Godot.SingletonBase
      * If manual session handling is enabled, there's a active session and SDK is initialized ("init" method will
      * start a session automatically), you can control the end of a session from here
      */
+
     public void endSession()
     {
         if(initialized) {
@@ -104,6 +106,85 @@ public class GodotGameAnalytics extends Godot.SingletonBase
         }    
     }
 
+    /**
+     * Progression Event Start. Use this when a player is starting a progression attempt
+     */
+
+    public void progressionStart_1(String progression01){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Start, progression01);
+        }
+    }
+
+    public void progressionStart_2(String progression01, String progression02){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Start, progression01, progression02);
+        }
+    }
+
+    public void progressionStart_3(String progression01, String progression02, String progression03){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Start, progression01, progression02, progression03);
+        }
+    }
+
+    /**
+     * Progression Event Fail. Use this when a player fails the attempt. When using a 3 tier hierarchy structure
+     * you can also pass an optional integer 
+     */
+
+    public void progressionFail_1(String progression01){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Fail, progression01);
+        }
+    }
+
+    public void progressionFail_2(String progression01, String progression02){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Fail, progression01, progression02);
+        }
+    }
+
+    public void progressionFail_3(String progression01, String progression02, String progression03){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Fail, progression01, progression02, progression03);
+        }
+    }
+
+    public void progressionFail_3_WithInt(String progression01, String progression02, String progression03, int score){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Fail, progression01, progression02, progression03, score);
+        }
+    }
+
+    /**
+     * Progression Event Complete. Use this when a player completes the attempt. When using a 3 tier hierarchy structure
+     * you can also pass an optional integer 
+     */
+
+    public void progressionComplete_1(String progression01){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Complete, progression01);
+        }
+    }
+
+    public void progressionComplete_2(String progression01, String progression02){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Complete, progression01, progression02);
+        }
+    }
+
+    public void progressionComplete_3(String progression01, String progression02, String progression03){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Complete, progression01, progression02, progression03);
+        }
+    }
+
+    public void progressionComplete_3_WithInt(String progression01, String progression02, String progression03, int score){
+        if(initialized) {
+            GameAnalytics.addProgressionEventWithProgressionStatus(GAProgressionStatus.Complete, progression01, progression02, progression03, score);
+        }
+    }
 
     /* Definitions
      * ********************************************************************** */
@@ -130,7 +211,18 @@ public class GodotGameAnalytics extends Godot.SingletonBase
             "sendCustomEventWithFloat",
             "manualSessionHandling",
             "startSession",
-            "endSession"
+            "endSession",
+            "progressionStart_1",
+            "progressionStart_2",
+            "progressionStart_3",
+            "progressionFail_1",
+            "progressionFail_2",
+            "progressionFail_3",
+            "progressionFail_3_WithInt",
+            "progressionComplete_1",
+            "progressionComplete_2",
+            "progressionComplete_3",
+            "progressionComplete_3_WithInt"
         });
 
         this.activity = activity;
